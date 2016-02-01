7882d83c175d6bd6ea034a30ce9ef670c989cc38
Ascris <antoinegarnier@hotmail.fr>
2016-02-01 21:22:55 +0100

    Lancement de la grosse appli exemple

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppExemple::Application.config.secret_token = '237ae99ba6b772a7f28ba75cd98a2aad5712195ad1abb33942f81d196d873e538903d966f3485773d348f8414fc10f0685efb986c06e32c7a5ce8aa7d800b05d'
