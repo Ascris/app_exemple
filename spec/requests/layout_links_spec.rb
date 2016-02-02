695cc9d8585a08f8bcfced0606b30b9d8e67cf4d
Ascris <antoinegarnier@hotmail.fr>
2016-02-02 21:51:09 +0100

    Routage fail

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -7,12 +7,12 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Accueil")
   end
 
-  it "devrait trouver une page Contact at '/Contact'" do
+  it "devrait trouver une page Contact a '/Contact'" do
     get '/Contact'
     response.should have_selector('title', :content => "Contact")
   end
 
-  it "should have an A_Propos page at '/A_Propos'" do
+  it "devrait trouver une page A_Propos a '/A_Propos'" do
     get '/A_Propos'
     response.should have_selector('title', :content => "A_Propos")
   end
